668da0e63900070d0542a5532869777f2a1ee74a
wl <2232127094@qq.com>
2021-06-04 14:30:53 +0800

    第一次进行数据的提交

---
+++ src/main/java/com/wl/SpringbootAnnoApplication.java
@@ -0,0 +1,11 @@
+package com.wl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringbootAnnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAnnoApplication.class, args);
+    }
+
+}
